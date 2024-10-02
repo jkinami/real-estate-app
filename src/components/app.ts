644c907a836e6d5e256391daf0e1a428a7603c1e
junichi kinami <junichi.kinami@gmail.com>
2024-10-02 21:39:36 +0900

    removed app.ts

--- src/components/app.ts
+++
@@ -1,4 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();  // Load environment variables from .env file
-const apiKey = process.env.RESAS_API_KEY;  // Retrieve the environment variable
-console.log('API Key:', apiKey);
