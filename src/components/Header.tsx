13d91d9ae82acd08be63bf984fcdd3f327ca59c4
junichi kinami <junichi.kinami@gmail.com>
2024-10-02 23:48:10 +0900

    updated logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <div className="h-[74px] w-full bg-white flex flex-wrap content-center sticky top-0">
       <div className="ml-[2px]">
         <img
-          src="/src/assets/logo-landit.png"
+          src="/src/assets/logo.jpg"
           alt="logo"
           width="233px"
           height="60px"
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,14 +2,7 @@ const Title = () => {
   return (
     <div className="mx-[40px] mt-[40px] h-[56px] w-auto border-b border-white border-opacity-20 text-white flex">
       <div className="text-4xl font-normal flex gap-[8px] w-[180px]">
-        <div className="flex-none mt-[12px]">
-          <img
-            src="/src/assets/chart-icon.png"
-            alt="chart"
-            width="32px"
-            height="30px"
-          />
-        </div>
+        <div className="flex-none mt-[12px]">{/* chat icon goes here */}</div>
         <span>取引価格</span>
       </div>
       <span className="ml-[59px] mt-[19px]">※取引面積1㎡あたり</span>
