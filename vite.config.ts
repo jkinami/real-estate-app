2a6d4ccfedb19739db8c67e7f6c4f827bd2eaadc
junichi kinami <junichi.kinami@gmail.com>
2024-10-04 11:19:36 +0900

    remove vitest from config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { config } from 'dotenv';
-// import { configDefaults } from 'vitest/config';
 
 config();
-
-// https://vitejs.dev/config/
 
 export default defineConfig({
   plugins: [react()],
